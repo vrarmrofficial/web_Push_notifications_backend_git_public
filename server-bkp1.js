fb6d24bd73258eeded384d244134b25e5cc79a65
azeem88 <mohammed.link.257@gmail.com>
2026-05-25 14:10:46 +0530

    Az-6
    -New generated json from service accounts and updated the server.js with .env configuration

--- server-bkp1.js
+++ server-bkp1.js
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -11,25 +9,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+const serviceAccount = require("./firebase-adminsdk.json");
+
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-  })
+  credential: admin.credential.cert(serviceAccount)
 });
 
 let userTokens = [];
 
 app.post("/save-token", (req, res) => {
   const { token } = req.body;
-
-  if (!token) {
-    return res.status(400).json({
-      success: false,
-      message: "Token is required"
-    });
-  }
 
   if (!userTokens.includes(token)) {
     userTokens.push(token);
@@ -42,19 +31,16 @@ app.post("/save-token", (req, res) => {
   });
 });
 
+
+//Add this temporary API in backend: Notification should instantly appear. while open "http://localhost:3000/test-notification"
 app.get("/test-notification", async (req, res) => {
-  try {
-    for (const token of userTokens) {
-      await sendPushNotification(token);
-    }
-
-    res.send("Test Notification sent");
-  } catch (error) {
-    console.error(error);
-
-    res.status(500).send("Failed to send notifications");
+  for (const token of userTokens) {
+    await sendPushNotification(token);
   }
+
+  res.send("Test Notification sent");
 });
+
 
 async function sendPushNotification(token) {
   const message = {
@@ -62,22 +48,21 @@ async function sendPushNotification(token) {
       title: "Campus Shoes",
       body: "Come back and explore our latest catalogue!"
     },
-    token
+    token: token
   };
 
   try {
     const response = await admin.messaging().send(message);
-
     console.log("Notification sent:", response);
   } catch (error) {
-    console.error("Firebase Error:", error);
+    console.error(error);
   }
 }
 
 /*
-Runs every 2 minutes
+Runs every 30 minutes
 */
-cron.schedule("*/2 * * * *", async () => {
+cron.schedule("*/1 * * * *", async () => {
   console.log("Sending reminders...");
 
   for (const token of userTokens) {
@@ -85,8 +70,6 @@ cron.schedule("*/2 * * * *", async () => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
